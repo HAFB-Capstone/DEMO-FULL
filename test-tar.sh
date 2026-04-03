29d62c1f3ce9306a21475834f4c50a7cb9b1b04f
sethbr11 <sethabrock@gmail.com>
2026-04-03 08:21:12 -0600

    Removed temporary files

--- test-tar.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p bak/apps/splunk-rolling-upgrade
-touch bak/apps/splunk-rolling-upgrade/file
-chown -R 1000:1000 bak
-mkdir -p etc/apps
-chown -R root:root etc/apps
-(cd bak; tar cf - *) | sudo -u root bash -c '(cd etc; tar xf -)'
-ls -ld etc/apps/splunk-rolling-upgrade
